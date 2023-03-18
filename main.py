febea8ded1d6dac60a3054afeda3c960a4a4c959
221RDB044 <124919320+EKerke@users.noreply.github.com>
2023-03-18 23:05:26 +0000

    3

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ def parallel_processing(n, m, data):
 
     assignJobs = [(0,0)] * m #inicializē masīvu ar m tuples kur uzkrāj thread indeksu un katra darba uzsākšanas laiku 
 
-    for i in range(m): #katru darbu piesaista thread un atjauno thread pabeigšanas laiku 
-        jobTime = data[i] #atrod darba laiku 
-        minFinishTime = min(finishTime) #atrod mazāko pabeigšanas laiku 
-        minThread = finishTime.index(minFinishTime) #atrod indeksu 
+    for i in range(m): 
+        jobTime = data[i] 
+        minFinishTime = finishTime[0] 
+        minThread = 0  
+        for j in range(n): 
+            if finishTime[j] < minFinishTime: 
+                minFinishTime = finishTime[j] 
+                minThread = j 
         assignJobs[i] = (minThread, finishTime[minThread]) 
         finishTime[minThread] += jobTime
 
-    output = [(assignJobs[i][0], assignJobs[i][0]) for i in range(m)] # izveido sarakstu ar izvadāmajiem pāriem
+    output = [(assignJobs[i][0], assignJobs[i][1]) for i in range(m)] # izveido sarakstu ar izvadāmajiem pāriem
 
     return output
 
@@ -26,8 +30,6 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
     n, m = map(int, input().split()) #reads the values of n and m from the keyboard
 
     # second line - data 
